3ff4beda068237f90ea4686910fbef5d50be7ab2
ykmro <ko.kansya@gmail.com>
2026-06-16 20:00:18 +0900

    Dockerのentrypoint修正

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,8 @@ set -e
 # マイグレーション実行
 bundle exec rails db:migrate
 
-# データが空の場合のみシード実行
-bundle exec rails runner "Situation.count == 0 && Rails.application.load_seed"
+# シード実行
+bundle exec rails db:seed
 
 # サーバー起動
 exec bundle exec rails s -p 3000 -b 0.0.0.0
